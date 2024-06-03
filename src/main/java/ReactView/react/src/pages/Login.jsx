d2d8d211e15773b340c16f905c649c29acb03988
etw0001 <122562296+etw0001@users.noreply.github.com>
2024-06-03 14:30:55 -0700

    implemented login page redesign

--- src/main/java/ReactView/react/src/pages/Login.jsx
+++ src/main/java/ReactView/react/src/pages/Login.jsx
@@ -2,75 +2,133 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
   Flex,
+  Box,
   Container,
   Stack,
   Text,
   Input,
   Checkbox,
   Button,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
 } from '@chakra-ui/react';
+import './Login.css';
 
 function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const [login, setLogin] = useState(['', '']);
   const navigate = useNavigate();
 
+  const usernameEmpty = isSubmitted && username === '';
+  const passwordEmpty = isSubmitted && password === '';
+
   function handleLoginClick(e) {
-    handleSubmit(e);
     /*Additional Login logic here*/
-    navigate('/home');
+    e.preventDefault();
+    setIsSubmitted(true);
+    if (username && password) {
+      navigate('/home');
+    }
   }
 
   function handleCreateAccountClick(e) {
-    handleSubmit(e);
     /*Additional Create Account logic here*/
-    navigate('/home');
+    e.preventDefault();
+    setIsSubmitted(true);
+    if (username && password) {
+      navigate('/home');
+    }
   }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setUsername('');
-    setPassword('');
-  };
+  function handleUsernameChange(e) {
+    setUsername(e.target.value);
+    setIsSubmitted(false);
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+    setIsSubmitted(false);
+  }
 
   return (
     <Flex width='100vw' height='100vh' align='center' justify='center'>
-      <Container style={{ maxWidth: '300px' }}>
-        <Stack spacing={2}>
-          <Text align='center' fontSize='20px' fontWeight={600}>
-            Welcome to Pantry Pals!
+      <Container
+        style={{
+          backgroundColor: '#F2E3D2',
+          maxWidth: '400px',
+          borderRadius: '16px',
+          padding: '48px',
+        }}>
+        <Stack spacing={2} marginTop='-12'>
+          <Box display='flex' justifyContent='center' marginBottom='-12'>
+            <img
+              src='/mochicook.png'
+              alt='Logo'
+              style={{ width: '200px', height: '200px' }}
+            />
+          </Box>
+          <Text
+            color='#856454'
+            className='title'
+            align='center'
+            fontSize='40px'
+            marginBottom='8px'>
+            PantryPal
           </Text>
-          <Input
-            variant='filled'
-            placeholder='Username'
-            size='sm'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <Input
-            variant='filled'
-            placeholder='Password'
-            size='sm'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Stack direction='row' justify='center'>
+          <FormLabel color='#856454' marginBottom='-1'>
+            Username
+          </FormLabel>
+          <FormControl isInvalid={usernameEmpty}>
+            <Input
+              backgroundColor='#BCA490'
+              variant='outline'
+              size='md'
+              value={username}
+              onChange={handleUsernameChange}
+            />
+            <FormErrorMessage marginTop='1'>
+              Username is required.
+            </FormErrorMessage>
+          </FormControl>
+          <FormLabel color='#856454' marginBottom='-1'>
+            Password
+          </FormLabel>
+          <FormControl isInvalid={passwordEmpty}>
+            <Input
+              backgroundColor='#BCA490'
+              variant='outline'
+              size='md'
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <FormErrorMessage marginTop='1'>
+              Password is required.
+            </FormErrorMessage>
+          </FormControl>
+          <Stack
+            direction='row'
+            justify='center'
+            marginTop='8'
+            marginBottom='0'
+            spacing='8'>
             <Button
-              flex='3'
+              flex='1'
               size='sm'
-              colorScheme='blue'
-              isDisabled={!username || !password}
+              color='white'
+              backgroundColor='#856454'
               onClick={handleLoginClick}>
-              Login
+              Sign In
             </Button>
             <Button
-              flex='5'
+              flex='1'
               size='sm'
-              colorScheme='green'
-              isDisabled={!username || !password}
+              color='white'
+              backgroundColor='#856454'
               onClick={handleCreateAccountClick}>
-              Create Account
+              Sign Up
             </Button>
           </Stack>
         </Stack>
