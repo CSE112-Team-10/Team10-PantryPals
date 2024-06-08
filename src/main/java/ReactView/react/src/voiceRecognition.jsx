58d6da665d4a4d5eb01154fafc196a289dd605e6
etw0001 <122562296+etw0001@users.noreply.github.com>
2024-06-07 21:11:51 -0700

    current progress on recording page

--- src/main/java/ReactView/react/src/voiceRecognition.jsx
+++ src/main/java/ReactView/react/src/voiceRecognition.jsx
@@ -1,7 +1,7 @@
 import 'regenerator-runtime';
 
 import React from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 import SpeechRecognition, {
   useSpeechRecognition,
@@ -41,25 +41,56 @@ const VoiceRecognition = (props) => {
     SpeechRecognition.startListening({ continuous: true });
   const stopListening = () => SpeechRecognition.stopListening();
 
-  const navigate = useNavigate()
+  const handleClick = () => {
+    if (listening) {
+      stopListening();
+    } else {
+      startListening();
+    }
+  };
+
+  const navigate = useNavigate();
 
   return (
     <Flex align='center' justify='center' width='full' height='full'>
-      <Container maxWidth='300px'>
-        <Stack>
+      <Container maxWidth='750px'>
+        <Stack spacing={2}>
+          <Text
+            color='#856454'
+            className='title'
+            align='center'
+            marginTop='-0.5em'
+            marginBottom='0.5em'
+            fontSize='40px'>
+            State Recipe Ingredients
+          </Text>
+          <img
+            src={listening ? '/mic.png' : '/micmuted.png'}
+            alt='Microphone Icon'
+            style={{
+              width: '200px',
+              height: '200px',
+              cursor: 'pointer',
+            }}
+            onClick={handleClick}
+          />
           <Stack direction='row' justify='center'>
-            <Button size='sm' onClick={startListening}>
-              Start Listening
+            <Button colorScheme='red' size='sm' onClick={resetTranscript}>
+              Reset
             </Button>
-            <Button size='sm' onClick={stopListening}>
-              Stop Listening
+            <Button
+              colorScheme='green'
+              size='sm'
+              onClick={() => onNavigate('OutputtedRecipe')}>
+              Generate
             </Button>
           </Stack>
-          <Button colorScheme='green' size='sm' onClick={() => onNavigate('OutputtedRecipe')}>
-            Generate
+          <Button size='sm'>Customize</Button>
+          <Button size='sm' onClick={startListening}>
+            Start Listening
           </Button>
-          <Button colorScheme='red' size='sm' onClick={resetTranscript}>
-            Reset
+          <Button size='sm' onClick={stopListening}>
+            Stop Listening
           </Button>
           <Text align='center' fontSize='14px' fontWeight={600}>
             Microphone: {listening ? 'on' : 'off'}
